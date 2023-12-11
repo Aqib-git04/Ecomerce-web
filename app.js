f565e9abd95922a1ed552b85f31ca2b0c5c3121c
Syed Aqib Rehan <syedaqib482002@gmail.com>
2023-12-12 01:00:58 +0500

    all web done

---
+++ app.js
@@ -0,0 +1,9 @@
+var list=document.getElementById("li");
+function openmenu(){
+ li.style.right="0";
+}
+
+var closeButton=document.getElementById("closeButton");
+function closemenu(){
+ li.style.right="-200px";
+}
